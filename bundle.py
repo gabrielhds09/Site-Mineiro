42336747f527d55157add2e53b9620702c43b307
Joao marcoso <gabrielhds06@gmail.com>
2026-02-03 10:07:51 -0300

    feat: mobile density refinement - mini-desktop experience v11

--- bundle.py
+++ bundle.py
@@ -19,7 +19,7 @@ try:
 
     # Replace Link Tag
     # We look for the specific link tag used in index.html
-    target_tag = '<link rel="stylesheet" href="css/style.css?v=10">'
+    target_tag = '<link rel="stylesheet" href="css/style.css?v=11">'
     
     if target_tag in html_content:
         final_html = html_content.replace(target_tag, style_block)
